2e86000c51b0978b8aad4e3c6d0ef6308c0ce9a3
Norvald Espedal <norvald@redpill-linpro.com>
2022-12-23 12:22:57 +0100

    Lecture 268

--- src/main/java/com/sfg/gateway/config/LoadBalancedRoutesConfig.java
+++ src/main/java/com/sfg/gateway/config/LoadBalancedRoutesConfig.java
@@ -13,12 +13,17 @@ public class LoadBalancedRoutesConfig {
     @Bean
     public RouteLocator loadBalancedRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(r-> r.path("/api/v1/beer", "/api/v1/beer/*", "/api/v1/beerUpc/*")
+                .route(r -> r.path("/api/v1/beer", "/api/v1/beer/*", "/api/v1/beerUpc/*")
                         .uri("lb://beer-service"))
                 .route(r -> r.path("/api/v1/customers/**")
                         .uri("lb://order-service"))
                 .route(r -> r.path("/api/v1/beer/*/inventory", "/api/v1/beerUpc/*/inventory")
+                        .filters(f -> f.circuitBreaker(c -> c.setName("inventoryCB")
+                                .setFallbackUri("forward:/inventory-failover")
+                                .setRouteId("inv-failover")))
                         .uri("lb://inventory-service"))
+                .route(r -> r.path("/inventory-failover/**")
+                        .uri("lb://inventory-failover"))
                 .build();
     }
 }
